d7c8c123d7c7485bedbcf9f7426a3f1fd92cb34d
Arce-PC\Arce <jorge.arce@inventic.com>
2019-07-06 23:50:50 -0400

    reajuste summernote

--- public/adminlte/plugins/summernote/src/js/bs3/settings.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-import ui from './ui';
-import '../base/settings.js';
-
-import '../../styles/summernote-bs3.scss';
-
-$.summernote = $.extend($.summernote, {
-  ui: ui,
-});
--- public/adminlte/plugins/summernote/src/js/lite/settings.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-import ui from './ui';
-import '../base/settings.js';
-
-import '../../styles/summernote-lite.scss';
-
-$.summernote = $.extend($.summernote, {
-  ui: ui,
-});
